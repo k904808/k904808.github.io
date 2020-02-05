413a2cb7dfe69d0d1d9d3816ba9c57432eb5a4d7
kay <k904808@gmail.com>
2020-02-05 23:25:26 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,20 +36,20 @@ self.__precacheManifest = [
     "url": "styles-117479406f6e61ad4793.js"
   },
   {
-    "url": "commons-ec254250f5977ab70825.js"
+    "url": "commons-afa0866fa7b2d0e9eab7.js"
   },
   {
     "url": "netlify-identity-widget-8e6f024872c0d48ae324.js"
   },
   {
-    "url": "app-cb375e9322e8a3a0403c.js"
+    "url": "app-aba93eda08eb41fee533.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-cf2fd0f92da7499f18b1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3e48097f813e5ef89b02b9d0d6bea03d"
+    "revision": "3ef1d4d0ae395c6ba45450c2179bcb6d"
   },
   {
     "url": "manifest.json",
@@ -150,7 +150,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-cb375e9322e8a3a0403c.js`))) {
+  if (!resources || !(await caches.match(`/app-aba93eda08eb41fee533.js`))) {
     return await fetch(event.request)
   }
 
