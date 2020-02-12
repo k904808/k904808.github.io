4ed427fb4f834719b900d38800bbc76287adc429
kay <k904808@gmail.com>
2020-02-12 19:31:11 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "styles-117479406f6e61ad4793.js"
   },
   {
-    "url": "commons-fa1afad554fe8acd64f7.js"
+    "url": "commons-d434d06d859fb7a488d6.js"
   },
   {
     "url": "netlify-identity-widget-8e6f024872c0d48ae324.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1881f8d1c13372299085e0f39ebed11c"
+    "revision": "228083cddb67ac7737ac75cab4d5d703"
   },
   {
     "url": "manifest.json",
